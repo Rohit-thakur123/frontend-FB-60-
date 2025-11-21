b6fb1dfb3631f7929f02f8c19ab37c7e7d4077c3
Rohit Singh <thakurrohitsingh.up85@gmail.com>
2025-11-21 20:07:58 +0530

    img_corousel

--- Assignments/javascript__code/03_make_carousel.js
+++ Assignments/javascript__code/03_make_carousel.js
@@ -7,7 +7,21 @@ let nextbtn=document.querySelector("#next");
     const images=["https://images.unsplash.com/photo-1641913625440-158406784a9f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687", "https://tse3.mm.bing.net/th/id/OIP.M95NmX9MTPcf4yG9X8efWgHaFj?pid=Api&P=0&h=180", "https://img.freepik.com/premium-photo/shree-krishna-illustration-janmashtami-generative-ai_756405-2066.jpg?w=2000", "https://cdn.pixabay.com/photo/2023/06/23/08/51/lord-krishna-8083043_640.png"];
 
     let index=0;
+    let len=images.length;
     
     prevbtn.addEventListener("click", ()=>{
-        
+        if(index===0){
+            index=len-1;
+        }else{
+            index--;
+        }
+        img.setAttribute("src", images[index]);
+    })
+    nextbtn.addEventListener("click", ()=>{
+        if(index===len-1){
+            index=0;    
+        }else{
+            index++;
+        }
+        img.setAttribute("src", images[index]);
     })
