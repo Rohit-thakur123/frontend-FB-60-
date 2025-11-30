dabf46b0dbf8cfff6c9ce618a5ca76de0e2126fb
Rohit Singh <thakurrohitsingh.up85@gmail.com>
2025-11-30 15:39:17 +0530

    error_handle

--- Assignments/javascript__code/06_make_calculator.js
+++ Assignments/javascript__code/06_make_calculator.js
@@ -3,20 +3,30 @@ let string="";
         let buttons=document.querySelectorAll(".button");
         console.log(buttons.length);
         Array.from(buttons).forEach((button)=>{
-            button.addEventListener("click", (e)=>{   
-                if(e.target.innerHTML == "="){
-                    string=eval(string);
-                    document.querySelector("input").value=string;     
-                }else if(e.target.innerHTML=="clear"){
-                    string="";
-                    document.querySelector("input").value=string;
-                }
-                else{
-                    string=string+e.target.innerHTML;
-                    document.querySelector("input").value=string;                   
+            button.addEventListener("click", (e)=>{ 
+                try{
+
+                    if(e.target.innerHTML == "="){
+                        string=eval(string);
+                        document.querySelector("input").value=string;     
+                    }else if(e.target.innerHTML=="clear"){
+                        string="";
+                        document.querySelector("input").value=string;
+                    }
+                    else{
+                        string=string+e.target.innerHTML;
+                        document.querySelector("input").value=string;                   
+                    }  
+                    
                 }  
-                
-                console.log(e.target.innerHTML);
+                catch(err){
+                    document.querySelector("input").value="syntax error";
+                    setTimeout(()=>{
+                        string="";
+                        document.querySelector("input").value=string;
+                        
+                    },1000);
+                }              
                 
             })
         })
